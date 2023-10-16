5b257fd2332d7dd4aad00d2d4f7373f17574028d
zahidhasanemran <zahid.emran@thealtcom.com>
2023-10-16 19:44:29 +0600

    For of loop, if else loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/wishItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'wisthlist';
+  title = 'Wishlist';
+  items: WishItem[] = [
+    // new WishItem('Learn Angular'),
+    // new WishItem('Have a cup of coffiee', true),
+    // new WishItem('Kill the birds :D '),
+  ];
 }
